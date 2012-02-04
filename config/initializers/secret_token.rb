8298eccb2c70950667cd5572005e2fed9200c761
Cora Korsman <corakorsman@gmail.com>
2012-02-04 17:26:18 +0100

    Generated Rails app for LibRoRian and added Cucumber and friends

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LibRoRian::Application.config.secret_token = '083c141867f516ff4bd65e8ba141190af76cf86da4b1e36ca573ea34f2a37f689e57ba3d57d0517f5d71ce2c388355e5821ebc0345e11266eea38ac0d5e21d06'
